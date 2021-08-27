77170abf04d76d44dec1b6afd69c881e82dbfbba
Emmanuel <opensoftweb@gmail.com>
2021-08-28 00:19:33 +0100

    add: supported chain constants

---
+++ src/data/constants/chains.ts
@@ -0,0 +1,13 @@
+export enum TESTNET {
+  CHAIN_ID = "testnet",
+  RPC_NODE = "https://rpc.testnet.near.org",
+  CONTRACT_NAME = "berryclub.testnet",
+  WALLET_URL = "https://wallet.testnet.near.org",
+}
+
+export enum MAINNET {
+  CHAIN_ID = "mainnet",
+  RPC_NODE = "https://rpc.mainnet.near.org",
+  CONTRACT_NAME = "berryclub.ek.near",
+  WALLET_URL = "https://wallet.near.org",
+}
