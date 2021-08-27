54884a37e5c0448c87316bdf2c2366920eb78899
Emmanuel <opensoftweb@gmail.com>
2021-08-27 22:43:47 +0100

    add: more dependencies

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from "react";
+import { PureComponent } from "react";
 import { Switch, Route } from "react-router-dom";
 
 export default class Routes extends PureComponent {
